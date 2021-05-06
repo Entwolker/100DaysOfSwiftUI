67122916394ba1859b859fa9273cbdc9db21f500
Kevin Hoàng <binxkevin@gmx.de>
2021-05-06 15:48:46 +0200

    Another project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kevin Hoàng on 06.05.21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
