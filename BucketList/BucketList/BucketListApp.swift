a010672f04e4487dc556326be9a656ec331bdd55
Kevin Hoàng <binxkevin@gmx.de>
2021-05-23 18:44:14 +0200

    Project 14

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kevin Hoàng on 21.05.21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
